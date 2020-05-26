924600fee21fd6e76f9af65d1cac74e88a14307a
Sofia Ling <sofialing@live.se>
2020-05-26 14:33:31 +0200

    Updated validation rules.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { login, register } = require('../controllers/auth_controller');
 const { validateToken } = require('../controllers/middlewares/auth');
-const { createRules } = require('../validation/profile');
+const { createAccount } = require('../validation/create');
 
 /* Login using JWT-token */
 router.post('/login', login);
 
 /* Register a new account */
-router.post('/register', createRules, register);
+router.post('/register', createAccount, register);
 
 /* Show users photos */
 router.use('/photos', [validateToken], require('./photos'))
--- validation/create.js
+++ validation/create.js
@@ -1,16 +1,23 @@
 /**
- * Validation rules to register new account
+ * Validation rules to create data
  */
 
 const { body } = require('express-validator');
 
-const createRules = [
+const createAccount = [
 	body('email').trim().isLength({ min: 8 }),
 	body('password').trim().isLength({ min: 8 }),
 	body('first_name').trim().isLength({ min: 2 }),
 	body('last_name').trim().isLength({ min: 2 }),
 ];
 
+const createPhoto = [
+	body('title').trim().notEmpty(),
+	body('url').trim().notEmpty(),
+	body('comment').optional().trim(),
+];
+
 module.exports = {
-	createRules,
+	createAccount,
+	createPhoto,
 };
