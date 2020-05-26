c0b739e6f1b7bb4e4fbf3a54c858f108e9c0ff1b
Sofia Ling <sofialing@live.se>
2020-05-26 14:40:53 +0200

    Updated validation rules for creating a new account (check if email already exists).

--- validation/create.js
+++ validation/create.js
@@ -3,9 +3,18 @@
  */
 
 const { body } = require('express-validator');
+const { User } = require('../models')
+
+const checkEmail = async value => {
+	const user = await new User({ email: value }).fetch({ require: false });
+
+	return user
+		? Promise.reject('Email already exists.')
+		: Promise.resolve();
+}
 
 const createAccount = [
-	body('email').trim().isLength({ min: 8 }),
+	body('email').trim().isLength({ min: 8 }).custom(checkEmail),
 	body('password').trim().isLength({ min: 8 }),
 	body('first_name').trim().isLength({ min: 2 }),
 	body('last_name').trim().isLength({ min: 2 }),
