1d1ec96db63fc0652c926a2be9d730324015c177
Sofia Ling <sofialing@live.se>
2020-05-27 09:26:54 +0200

    Updated Album model.

--- models/Album.js
+++ models/Album.js
@@ -3,13 +3,20 @@
  */
 module.exports = bookshelf => {
 	return bookshelf.model('Album', {
-		tableName: 'albums',
 		hidden: ['user_id'],
+		requireFetch: false,
+		tableName: 'albums',
 		user() {
 			return this.belongsTo('User');
 		},
 		photos() {
 			return this.belongsToMany('Photo');
 		}
-	});
+	},
+		{
+			fetchById(id, user_id, fetchOptions = {}) {
+				return new this({ id, user_id }).fetch(fetchOptions);
+			},
+		}
+	);
 };
